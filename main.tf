4231524255f54ea0ce1ff2cbd1eaa2c7e9643480
GarimaGitRepo <116530909+GarimaGitRepo@users.noreply.github.com>
2023-04-05 17:25:48 -0400

    fixed dev vpc error

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-
+  enable_dns_hostnames = true
 
   tags = {
     Terraform = "true"
@@ -37,6 +37,7 @@ resource "aws_instance" "blog" {
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  subnet_id = module.vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
@@ -48,7 +49,8 @@ module "blog_sg" {
   version = "4.17.1"
   name = "blog_new"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.vpc.vpc_id
+
   
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
